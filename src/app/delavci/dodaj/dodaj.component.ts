6f02a89c93a82104ea9f4cf623de0fc948246025
eager39 <xxxzanxxx@gmail.com>
2019-01-30 16:18:28 +0100

    idk

--- src/app/delavci/dodaj/dodaj.component.ts
+++ src/app/delavci/dodaj/dodaj.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-dodaj',
   templateUrl: './dodaj.component.html',
@@ -8,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class DodajComponent implements OnInit {
 
   constructor() { }
+  delavec = new FormGroup({
+    ime: new FormControl(''),
+    priimek: new FormControl(''),
+    kraj_rojstva:new FormControl(""),
+    drzava_rojstva:new FormControl(""),
+    maticna_st:new FormControl(""),
+    drzavljanstvo:new FormControl(""),
+    stalni_naslov:new FormControl(""),
+    zacasni_naslov:new FormControl(""),
+    izobrazba:new FormControl(""),
+    invalidnost:new FormControl(""),
+    kat_inv:new FormControl(""),
+    delna_upok:new FormControl(""),
+    ime_delo_dop:new FormControl(""),
+    naslov_delo_dop:new FormControl(""),
 
+   
+  });
   ngOnInit() {
   }
 
